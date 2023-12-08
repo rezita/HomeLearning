7e4c437a14f3fda6e7417074622b39e2b9e7c7c5
unknown <zita.reiner@gmail.com>
2023-12-08 10:45:10 +0000

    add TabLayout for mainActivity

--- app/src/main/java/com/github/rezita/homelearning/view/MainActivity.kt
+++ app/src/main/java/com/github/rezita/homelearning/view/MainActivity.kt
@@ -3,63 +3,59 @@ package com.github.rezita.homelearning.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.github.rezita.homelearning.R
 import com.github.rezita.homelearning.databinding.ActivityMainBinding
-import com.github.rezita.homelearning.network.WordsProvider
+import com.github.rezita.homelearning.view.fragments.AdminTabFragment
+import com.github.rezita.homelearning.view.fragments.ErikTabFragment
+import com.github.rezita.homelearning.view.fragments.MarkTabFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 
-@Suppress("UNCHECKED_CAST")
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private var userName : String? = null
-    private var wordsProvider: WordsProvider? = null
+    lateinit var binding: ActivityMainBinding
+    private var userName: String? = null
+
+
+    var adapter: MainTabAdapter? = null
+
+    val labels = arrayOf(
+        R.string.main_activity_tab1,
+        R.string.main_activity_tab2,
+        R.string.main_activity_tab3
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val viewPager = binding.pager
+        val tabLayout = binding.tabLayout
+
+        val adapter = MainTabAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = getString(labels[position])
+        }.attach()
         //removeUserName()
-        wordsProvider = WordsProvider(applicationContext)
         setUserName()
-
-        prepareView()
-        binding.btnStartSpelling.setOnClickListener{startErikSpelling()}
-        binding.btnAddSpellingWords.setOnClickListener{addNewWords()}
-        binding.btnStartReading.setOnClickListener { startReading() }
-        binding.btnStartIrregular.setOnClickListener { startIrregularVerbs() }
-        binding.btnRestoreSpelling.setOnClickListener{confirmRestoreSpellingFromLogs()}
     }
 
-    private fun prepareView(){
-        setPrBarVisibility(false)
-    }
-
-    private fun startErikSpelling(){
-        val intent = Intent(this, SpellingActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun startReading(){
-        val intent = Intent(this, ReadingActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun startIrregularVerbs(){
-        val intent = Intent(this, IrregularVerbsActivity::class.java)
-        startActivity(intent)
-    }
-/*
-    private fun removeUserName(){
-        val prefs = this.getSharedPreferences("", Context.MODE_PRIVATE)
-        prefs.edit().remove(R.string.preferences_user_name.toString()).apply()
-        prefs.edit().remove("SecretCode").apply()
-    }
-*/
-    private fun setUserName(){
+    /*
+        private fun removeUserName(){
+            val prefs = this.getSharedPreferences("", Context.MODE_PRIVATE)
+            prefs.edit().remove(R.string.preferences_user_name.toString()).apply()
+            prefs.edit().remove("SecretCode").apply()
+        }
+    */
+    private fun setUserName() {
         userName = readSavedUser()
         if (userName == null) {
             val intent = Intent(this, RegisterActivity::class.java)
@@ -67,41 +63,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun readSavedUser() : String? {
+    private fun readSavedUser(): String? {
         val prefs = this.getSharedPreferences("", Context.MODE_PRIVATE)
         return prefs.getString(R.string.preferences_user_name.toString(), null)
     }
 
-    private fun addNewWords() {
-        val intent = Intent(this, UploadSpellingWordsActivity::class.java)
-        startActivity(intent)
-    }
+    inner class MainTabAdapter(fragment: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fragment, lifecycle) {
 
-    private fun confirmRestoreSpellingFromLogs(){
-        AlertDialog.Builder(this)
-            .setMessage(getString(R.string.confirm_restore_from_log))
-            .setPositiveButton(getString(R.string.yes_button)) { _, _ -> restoreSpellingFromLogs() }
-            .setNegativeButton(getString(R.string.no_button)) { dialogInterface, _ -> dialogInterface.cancel() }
-            .setTitle(getString(R.string.restore_dialog_title))
-            .create()
-            .show()
-    }
+        override fun getItemCount(): Int = labels.size
 
-
-    private fun restoreSpellingFromLogs(){
-        setPrBarVisibility(true)
-        wordsProvider?.restoreSpellingWordsFromLogs { response -> onSpellingRestored(response) }
-    }
-
-    private fun onSpellingRestored(response: String){
-        setPrBarVisibility(false)
-        Toast.makeText(this, response, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun setPrBarVisibility(isVisible : Boolean){
-        when(isVisible){
-            true -> binding.mainProgressbar.root.visibility = View.VISIBLE
-            false -> binding.mainProgressbar.root.visibility = View.GONE
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> ErikTabFragment()
+                1 -> MarkTabFragment()
+                else -> AdminTabFragment()
+            }
         }
     }
 }
