79723e361b12da4ae3f79b4f5123d2d404d072c0
unknown <zita.reiner@gmail.com>
2024-05-09 09:51:40 +0100

    add keyboardAction and keyboardOptions for EditScreen

--- app/src/main/java/com/github/rezita/homelearning/ui/screens/uploadwords/edit/EditScreen.kt
+++ app/src/main/java/com/github/rezita/homelearning/ui/screens/uploadwords/edit/EditScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -25,8 +27,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -124,6 +132,7 @@ private fun EditTextField(
     error: Int? = null,
     maxLength: Int? = null
 ) {
+    val focusManager = LocalFocusManager.current
     Column(
         horizontalAlignment = Alignment.Start,
         modifier = Modifier
@@ -155,7 +164,19 @@ private fun EditTextField(
             trailingIcon = trailingIcon,
             isError = error != null,
             singleLine = true,
-            colors = colors
+            colors = colors,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrect = false,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    // Move the text indicator's focus to the next field or the drop down menu
+                    focusManager.moveFocus(FocusDirection.Next)
+                },
+            ),
         )
     }
 }
