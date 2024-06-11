bc3f1d9e34584b2e308dd3a45878e8a197fc1435
unknown <zita.reiner@gmail.com>
2024-06-11 14:34:41 +0100

    ad semantics matchers

---
+++ app/src/androidTest/java/com/github/rezita/homelearning/ui/util/HomeLearningSemanticsMatcher.kt
@@ -0,0 +1,16 @@
+package com.github.rezita.homelearning.ui.util
+
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isEnabled
+
+fun withRole(role: Role) = SemanticsMatcher.expectValue(
+    SemanticsProperties.Role, role
+)
+
+fun properButton(title: String) =
+    hasText(title).and(withRole(Role.Button)).and(isEnabled()).and(hasClickAction())
+
