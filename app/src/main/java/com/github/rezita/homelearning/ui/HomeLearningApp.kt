a638e61c1a3e44ee2e5f5df7dac2e21a715fd309
unknown <zita.reiner@gmail.com>
2024-06-21 12:56:37 +0100

    remove unnecessary HomeLearningTheme

--- app/src/main/java/com/github/rezita/homelearning/ui/HomeLearningApp.kt
+++ app/src/main/java/com/github/rezita/homelearning/ui/HomeLearningApp.kt
@@ -2,16 +2,12 @@ package com.github.rezita.homelearning.ui
 
 import androidx.compose.runtime.Composable
 import com.github.rezita.homelearning.navigation.HomeLearningNavigation
-import com.github.rezita.homelearning.ui.theme.HomeLearningTheme
 
 @Composable
 fun HomeLearningApp(
     appState: HomeLearningAppState
 ) {
-    HomeLearningTheme {
-        HomeLearningNavigation(
-            homeLearningAppState = appState,
-        )
-
-    }
+    HomeLearningNavigation(
+        homeLearningAppState = appState,
+    )
 }
