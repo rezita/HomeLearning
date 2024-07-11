d07963ad77e8fbf6a1e0f180161a1c19bb1390e8
unknown <zita.reiner@gmail.com>
2024-07-11 08:08:45 +0100

    topAppBar title position

--- app/src/main/java/com/github/rezita/homelearning/ui/screens/common/LearningAppBar.kt
+++ app/src/main/java/com/github/rezita/homelearning/ui/screens/common/LearningAppBar.kt
@@ -4,12 +4,12 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -30,7 +30,7 @@ fun LearningAppBar(
     actions: @Composable RowScope.() -> Unit = {},
 
     ) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         modifier = modifier,
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary,
